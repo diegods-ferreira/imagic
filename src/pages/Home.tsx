5e1212eb627bf8bb74a22f1c2b269453935d9351
Diego Ferreira <diegosf1705@gmail.com>
2022-06-10 16:54:30 -0300

    Displaying images...

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VStack } from '@chakra-ui/react';
+import { Box, Image, SimpleGrid, VStack } from '@chakra-ui/react';
 
 import { NavBar } from '../components/nav-bar.component';
 import { Hero } from '../components/hero.component';
@@ -10,6 +10,13 @@ export const HomePage: React.FC = () => {
       <NavBar />
 
       <Hero />
+
+      <SimpleGrid columns={3} spacing="16px" w="100%" maxW="1080px" border="1px solid red" py="24px">
+        <Box borderRadius="16px" overflow="hidden">
+          <Image src="https://source.unsplash.com/random/1920x1080/?landscape" w="100%" />
+          <h1>Teste</h1>
+        </Box>
+      </SimpleGrid>
     </VStack>
   );
 };
